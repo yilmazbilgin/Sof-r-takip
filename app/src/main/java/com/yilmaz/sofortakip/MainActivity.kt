7ff6a83286beace960a4afb40b0f387d36ba0d01
yilmazbilgin <ylmzbsqn@gmail.com>
2026-08-16 01:37:39 +0300

    Update MainActivity.kt

--- app/src/main/java/com/yilmaz/sofortakip/MainActivity.kt
+++ app/src/main/java/com/yilmaz/sofortakip/MainActivity.kt
@@ -29,6 +29,7 @@ import androidx.compose.material.icons.filled.Route
 import androidx.compose.material.icons.filled.Speed
 import androidx.compose.material.icons.filled.Stop
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -90,10 +91,12 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(
+    ExperimentalFoundationApi::class,
+    ExperimentalMaterial3Api::class
+)
 @Composable
 fun SoforTakip() {
-
     val context = LocalContext.current
 
     var seferler by remember {
