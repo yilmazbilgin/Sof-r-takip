b0dd57e3f58acf2547967ec0f86db2f46b701251
yilmazbilgin <ylmzbsqn@gmail.com>
2026-08-20 17:55:35 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,6 @@ dependencies {
 implementation("androidx.compose.material:material-icons-extended")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
-implementation("io.coil-kt.coil3:coil-compose:3.3.0")
-implementation("io.coil-kt.coil3:coil-network-okhttp:3.3.0")
+implementation("io.coil-kt.coil3:coil-compose:3.2.0")
+implementation("io.coil-kt.coil3:coil-network-okhttp:3.2.0")
 }
