87f9c09c3113b91287319b4a7406d3b861c1c006
yilmazbilgin <ylmzbsqn@gmail.com>
2026-08-20 16:04:19 +0300

    Update MainActivity.kt

--- app/src/main/java/com/yilmaz/sofortakip/MainActivity.kt
+++ app/src/main/java/com/yilmaz/sofortakip/MainActivity.kt
@@ -37,6 +37,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
